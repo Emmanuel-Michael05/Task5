aa0c5c81a7fc7f1ad6aa7c8582715bc9f7dd2430
Emmanuel Michael Ikechukwu <162467554+Emmanuel-Michael05@users.noreply.github.com>
2024-11-08 14:06:50 +0100

    Add project files.

---
+++ Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greet("Alice");
+        }
+       
+        string name;
+
+        public static void Greet(string name) 
+        {
+          
+            Console.WriteLine($"Hello {name}!");
+        }
+
+        
+    }
+}
